6597d0d0cf02ae9c64e15fd3bc50a694310fad48
Gregory Singleton <yadig69design@gmail.com>
2020-08-17 17:26:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,10 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete(Array)
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
